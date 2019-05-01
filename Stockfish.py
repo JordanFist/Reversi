c7643131b73b2125ca02194bfc96b98a9fab05e8
Lucas Barros de Assis <lbassis@inf.ufrgs.br>
2019-05-01 02:11:54 +0200

    now with a depth parameter which works

--- Stockfish.py
+++ Stockfish.py
@@ -6,12 +6,18 @@ from numpy import inf
 
 class Stockfish:
 
-    def return_best_move(self, b):
+
+    current_depth = 0
+    max_depth = 0
+    
+    def return_best_move(self, b, depth):
+
+        self.current_depth = 0
+        self.max_depth = depth
+        
         alpha = -inf
         beta = inf
         best = alpha
-
-  #      print("no best move o player é o " + str(b._nextPlayer))
 
         for i in b.legal_moves():
             b.push(i)
@@ -31,7 +37,9 @@ class Stockfish:
 
     def min_max(self, b, alpha, beta): # c'est au adversaire de jouer
 
-        if b.is_game_over():
+        self.current_depth += 1
+        
+        if b.is_game_over() or self.current_depth >= self.max_depth:
  #           print("a heuristica achou " + str(b.heuristique(b._nextPlayer)) + " pro " + str(b._nextPlayer))
             return -b.heuristique()
                 
@@ -48,7 +56,9 @@ class Stockfish:
 
     def max_min(self, b, alpha, beta): # c'est a toi de jouer
 
-        if b.is_game_over():
+        self.current_depth += 1
+        
+        if b.is_game_over() or self.current_depth >= self.max_depth:
 #            print("a heuristica achou " + str(b.heuristique(b._nextPlayer)) + " pro " + str(b._nextPlayer))
             return b.heuristique()
 
