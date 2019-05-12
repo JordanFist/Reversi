c9a469444719840bc2f138ea098efda147c1b831
Lucas Barros de Assis <lbassis@inf.ufrgs.br>
2019-05-12 02:11:44 +0200

    trying to make some heuristics

--- Stockfish2.py
+++ Stockfish2.py
@@ -1,49 +1,67 @@
 from Reversi import Board
-from time import time
+import time
 from random import randint,choice
 from numpy import inf
 
 
-class Stockfish:
-
-
+class Stockfish2:
     
-    current_depth = 0
-    max_depth = 0
-
-    pieces = 4
+    _current_depth = 0
+    _max_depth = 0
+    _turn = 0
+    _mid_game = 60 # quand on change de strategie (il faut bien verifier ce numero)
+    _max_time = 3
+    
 
     def __init__(self, player):
         self.player = player
 
         
     def getPlayerMove(self, b):
-        return self.best_move(b, 10)
 
+        depth = 30
+
+        if self._turn >= 30:
+            _depth = 15
+
+        duration = 0
+        while (duration < self._max_time/1.75):
+            start = time.time()
+            best = self.best_move(b, depth)
+            end = time.time()
+            duration += end-start
+            depth += 1
+
+        print("fui ate " + str(depth) + " em " + str(duration))
+        return best
+        
     def corners(self, b):
         result = 0
         corner = b._boardsize-1
+        board = b._board
         
-        if b[0][0] == player:
+        if board[0][0] == self.player:
             result += 1
-        if b[0][corner] == player:
+        if board[0][corner] == self.player:
             result += 1
-        if b[corner][0] == player:
+        if board[corner][0] == self.player:
             result += 1
-        if b[corner][corner] == player:
+        if board[corner][corner] == self.player:
             result += 1
 
         return result
 
-    def amount_legal_moves(self, b):
+    def mobility(self, b):
         return len(b.legal_moves())
+
+    def heuristique(self, b):
+        return self.mobility(b)+self.corners(b)
 
     def best_move(self, b, depth):
 
         current_pieces = b.get_nb_pieces()
-        self.pieces = current_pieces[0]+current_pieces[1]
-        self.current_depth = 0
-        self.max_depth = depth
+        self._current_depth = 0
+        self._max_depth = depth
         
         alpha = -inf
         beta = inf
@@ -59,7 +77,7 @@ class Stockfish:
             if beta <= alpha:
                 break
 
-        print("the best value for " + str(b._nextPlayer) + " is " + str(best))
+        #print("the best value for " + str(b._nextPlayer) + " is " + str(best))
         return move
 
     def random_move(b):
@@ -67,14 +85,13 @@ class Stockfish:
 
     def min_max(self, b, alpha, beta): # c'est au adversaire de jouer
 
-        current_pieces = b.get_nb_pieces()
-        if current_pieces[0]+current_pieces[1] > self.pieces:
-            self.current_depth += 1
-            self.pieces = current_pieces[0]+current_pieces[1]
+        if len(b.stack) > self._pieces:
+            self._current_depth += 1
+            self._pieces = current_pieces[0]+current_pieces[1]
         
-        if b.is_game_over() or self.current_depth >= self.max_depth:
+        if b.is_game_over() or self._current_depth >= self._max_depth:
  #           print("a heuristica achou " + str(b.heuristique(b._nextPlayer)) + " pro " + str(b._nextPlayer))
-            return -b.heuristique()
+            return -self.heuristique(b)
                 
         best = inf
         for i in b.legal_moves():
@@ -89,14 +106,13 @@ class Stockfish:
 
     def max_min(self, b, alpha, beta): # c'est a toi de jouer
         
-        current_pieces = b.get_nb_pieces()
-        if current_pieces[0]+current_pieces[1] > self.pieces:
-            self.current_depth += 1
+        if len(b.stack) > self._pieces:
+            self._current_depth += 1
             self.pieces = current_pieces[0]+current_pieces[1]
             
-        if b.is_game_over() or self.current_depth >= self.max_depth:
+        if b.is_game_over() or self._current_depth >= self._max_depth:
 #            print("a heuristica achou " + str(b.heuristique(b._nextPlayer)) + " pro " + str(b._nextPlayer))
-            return b.heuristique()
+            return self.heuristique(b)
 
         best = -inf
         for i in b.legal_moves():
