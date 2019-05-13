927d1082b9fa7dd496aa462b541dbd8b027c04d6
Lucas Barros de Assis <lbassis@inf.ufrgs.br>
2019-05-13 23:10:11 +0200

    iterative deepening really working

--- Stockfish2.py
+++ Stockfish2.py
@@ -1,67 +1,56 @@
 from Reversi import Board
 import time
+import math
 from random import randint,choice
 from numpy import inf
 
 
 class Stockfish2:
     
-    _current_depth = 0
-    _max_depth = 0
-    _turn = 0
-    _mid_game = 60 # quand on change de strategie (il faut bien verifier ce numero)
-    _max_time = 3
+    _initial_depth = 2
+    _last_duration = 0
     
+    def __init__(self, player, size):
+        self._player = player
+        self._remaining_turns = size**2/2
+        self._remaining_time = 300
+        self._max_time = self._remaining_time/self._remaining_turns
+        self._coef = 3
 
-    def __init__(self, player):
-        self.player = player
+    def update_time(duration):
+        self._remaining_time -= duration
+        self._remaining_turns -= 1
+        self._max_time = self._remaining_time/self._remaining_turns
 
         
     def getPlayerMove(self, b):
 
-        depth = 30
-
-        if self._turn >= 30:
-            _depth = 15
-
         duration = 0
-        while (duration < self._max_time/1.75):
+        depth = self._initial_depth
+        expo = 0
+        estimated_duration = 0
+        
+        while (estimated_duration < self._max_time):
             start = time.time()
             best = self.best_move(b, depth)
             end = time.time()
             duration += end-start
             depth += 1
+            expo = math.log(duration, depth)
+            #estimated_duration = duration + depth**(expo+1)
+            #print("avec la prochaine ça prendrait " + str(estimated_duration))
+            #self._coef = 2*duration/((depth-1)**expo) # 2 to be verified
 
-        print("fui ate " + str(depth) + " em " + str(duration))
+        depth -= 1
+        print(str(50-self._remaining_turns) + ": " +str(depth)+ " in " + str(duration)+ "s")
+        #print("took " + str(duration) + "s at depth " + str(depth-1) + " which was " + str(self._coef) + "*" + str(depth-1) + "^" + str(expo))
+
+        update_time(duration)
+
+        #print("turn " + str(50-self._remaining_turns) + " and i still have " + str(self._remaining_time) + "(" + str(self._max_time) + " per turn)")        
         return best
-        
-    def corners(self, b):
-        result = 0
-        corner = b._boardsize-1
-        board = b._board
-        
-        if board[0][0] == self.player:
-            result += 1
-        if board[0][corner] == self.player:
-            result += 1
-        if board[corner][0] == self.player:
-            result += 1
-        if board[corner][corner] == self.player:
-            result += 1
-
-        return result
-
-    def mobility(self, b):
-        return len(b.legal_moves())
-
-    def heuristique(self, b):
-        return self.mobility(b)+self.corners(b)
 
     def best_move(self, b, depth):
-
-        current_pieces = b.get_nb_pieces()
-        self._current_depth = 0
-        self._max_depth = depth
         
         alpha = -inf
         beta = inf
@@ -69,7 +58,7 @@ class Stockfish2:
 
         for i in b.legal_moves():
             b.push(i)
-            best = max(best, self.min_max(b, alpha, beta))
+            best = max(best, self.min_max(b, alpha, beta, depth-1))
             if (best > alpha):
                 move = i
             alpha = max(best, alpha)
@@ -77,26 +66,18 @@ class Stockfish2:
             if beta <= alpha:
                 break
 
-        #print("the best value for " + str(b._nextPlayer) + " is " + str(best))
         return move
 
-    def random_move(b):
-        return choice(b.legal_moves())
 
-    def min_max(self, b, alpha, beta): # c'est au adversaire de jouer
-
-        if len(b.stack) > self._pieces:
-            self._current_depth += 1
-            self._pieces = current_pieces[0]+current_pieces[1]
+    def min_max(self, b, alpha, beta, depth): # c'est au adversaire de jouer
         
-        if b.is_game_over() or self._current_depth >= self._max_depth:
- #           print("a heuristica achou " + str(b.heuristique(b._nextPlayer)) + " pro " + str(b._nextPlayer))
+        if b.is_game_over() or depth == 0:
             return -self.heuristique(b)
                 
         best = inf
         for i in b.legal_moves():
             b.push(i)
-            best = min(best, self.max_min(b, alpha, beta))
+            best = min(best, self.max_min(b, alpha, beta, depth-1))
             beta = min(best, beta)
             b.pop()
             if beta <= alpha:
@@ -104,23 +85,47 @@ class Stockfish2:
             
         return best
 
-    def max_min(self, b, alpha, beta): # c'est a toi de jouer
+    def max_min(self, b, alpha, beta, depth): # c'est a toi de jouer
         
-        if len(b.stack) > self._pieces:
-            self._current_depth += 1
-            self.pieces = current_pieces[0]+current_pieces[1]
-            
-        if b.is_game_over() or self._current_depth >= self._max_depth:
-#            print("a heuristica achou " + str(b.heuristique(b._nextPlayer)) + " pro " + str(b._nextPlayer))
+        if b.is_game_over() or depth == 0:
             return self.heuristique(b)
 
         best = -inf
         for i in b.legal_moves():
             b.push(i)
-            best = max(best, self.min_max(b, alpha, beta))
+            best = max(best, self.min_max(b, alpha, beta, depth))
             alpha = max(best, alpha)
             b.pop()
             if beta <= alpha:
                 break
         return best
 
+
+    def corners(self, b):
+        result = 0
+        corner = b._boardsize-1
+        board = b._board
+        
+        if board[0][0] == self._player:
+            result += 3
+        if board[0][corner] == self._player:
+            result += 3
+        if board[corner][0] == self._player:
+            result += 3
+        if board[corner][corner] == self._player:
+            result += 3
+
+        return result
+
+    def mobility(self, b):
+        return len(b.legal_moves())
+
+    def disks(self, b):
+        player = self._nextPlayer
+        if player is self._WHITE:
+            return self._nbWHITE - self._nbBLACK
+        return self._nbBLACK - self._nbWHITE
+
+
+    def heuristique(self, b):
+        return self.mobility(b)+self.corners(b)+self.disks(b)
