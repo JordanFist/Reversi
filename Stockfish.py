04c6252e236e9c027c5b2f3aa5fd1c68d883ce41
Lucas Barros de Assis <lucas.barros_de_assis@enseirb-matmeca.fr>
2019-05-20 16:03:19 +0200

    time management

--- Stockfish.py
+++ Stockfish.py
@@ -8,11 +8,10 @@ from Openings import Openings
 from playerInterface import *
 
 
-
 class Stockfish(PlayerInterface):
 
     _initial_depth = 1
-    _last_duration = 0
+    _max_duration = 6
 
     UNKNOWN_MAX = inf
     UNKNOWN_MIN = -inf
@@ -23,8 +22,8 @@ class Stockfish(PlayerInterface):
     def __init__(self):
 
         self._board = Reversi.Board(10)
-        self._remaining_turns = 96
-        self._remaining_time = 300
+        self._remaining_turns = 48
+        self._remaining_time = 290
         self._max_time = self._remaining_time/self._remaining_turns
         self._dict = {}
         self._openings = Openings(10)
@@ -40,28 +39,42 @@ class Stockfish(PlayerInterface):
 
         duration = 0
         depth = self._initial_depth
-        expo = 0
-        estimated_duration = 0
+        estimated_duration = 0.01 # pour la premiere estimation on met une valeur approxime
 
         openingMove = self._openings.getOpeningMove(self._board)
         if (openingMove != None):
             self._board.push(openingMove)
             move = (openingMove[1], openingMove[2])
+            self._turn += 1
             return move
 
-        while (depth < 4):
+        
+        while estimated_duration < self._max_duration and depth <= 20: #s'il arrive a 20 c'est parce que le jeu est termine et il cherche rien
             start = time.time()
             best = self.search(self._board, depth)
+            self._best_move = best
             end = time.time()
             duration += end-start
+            #print(duration, estimated_duration)
             depth += 1
-            expo = math.log(duration, depth)
-            estimated_duration = duration + depth**(expo+0.5)
+            base = duration**(1/depth)
+            coef = duration/estimated_duration
+            estimated_duration = duration + coef*base**depth
+
+        self._turn += 1
+        if self._turn >= 30:
+            if self._turn >= 48:
+                self._max_duration = self._remaining_time/2
+            else:
+                self._max_duration = self._remaining_time/(48-self._turn)
+
+            print("max maintenant est " + str(self._max_duration))
 
         self.update_time(duration)
-
+        print("depth: " + str(depth-1) + " en " + str(duration))
         self._board.push(best)
         move = (best[1],best[2])
+        print("total time: " + str(300-self._remaining_time))
         return move
 
     def playOpponentMove(self, x,y):
@@ -100,6 +113,7 @@ class Stockfish(PlayerInterface):
             else:
                 return self.UNKNOWN_MIN
 
+
     def order_moves(self, b, before):
 
 
@@ -121,7 +135,6 @@ class Stockfish(PlayerInterface):
         alpha = -inf
         beta = inf
         best = alpha
-        self._turn += 1
 
         moves = b.legal_moves()
         ordered_moves = self.order_moves(b, moves)
@@ -211,84 +224,84 @@ class Stockfish(PlayerInterface):
         return len(b.legal_moves()*2)
 
 
-    def edge_stability(self,b):
+    # def edge_stability(self,b):
 
-        def is_corner(pos):
-            if pos[0]==0 and (pos[1] == 0 or pos[1] == b._boardsize):
-                return True
-            if pos[b._boardsize]==0 and (pos[1] == 0 or pos[1] == b._boardsize):
-                return True
-            return False
+    #     def is_corner(pos):
+    #         if pos[0]==0 and (pos[1] == 0 or pos[1] == b._boardsize):
+    #             return True
+    #         if pos[b._boardsize]==0 and (pos[1] == 0 or pos[1] == b._boardsize):
+    #             return True
+    #         return False
 
-        def probability(edge, pos):
-            if is_corner(pos):
-                return 0
-            moves = legal_moves()
-            for move in moves:
-                if move[1:] in edge[0]:
-                    return 1
-            #all positions of the board
-            positions =[ [[j,i] for i in range(b._boardsize)] for j in range(b._boardsize)]
-            radius = b._boardsize/5
-            #tmp has all neighboring positions even those outside the board
-            tmp=[]
-            for i in range(-radius, radius+1):
-                for j in range(-radius, radius+1):
-                    if not (i==0 and j==0):
-                        tmp.append([pos[0]+i, pos[1]+j])
-            #neighboring positions withing the board
-            neighbords = [square for square in tmp if square in positions]
-            s = 0
-            for sqr in neighbors:
-                if b._board[sqr[0], sqr[1]] == b._flip():
-                    s+=1
-            return s/( (2*radius+1) * (radius+1) -1 )
+    #     def probability(edge, pos):
+    #         if is_corner(pos):
+    #             return 0
+    #         moves = legal_moves()
+    #         for move in moves:
+    #             if move[1:] in edge[0]:
+    #                 return 1
+    #         #all positions of the board
+    #         positions =[ [[j,i] for i in range(b._boardsize)] for j in range(b._boardsize)]
+    #         radius = b._boardsize/5
+    #         #tmp has all neighboring positions even those outside the board
+    #         tmp=[]
+    #         for i in range(-radius, radius+1):
+    #             for j in range(-radius, radius+1):
+    #                 if not (i==0 and j==0):
+    #                     tmp.append([pos[0]+i, pos[1]+j])
+    #         #neighboring positions withing the board
+    #         neighbords = [square for square in tmp if square in positions]
+    #         s = 0
+    #         for sqr in neighbors:
+    #             if b._board[sqr[0], sqr[1]] == b._flip():
+    #                 s+=1
+    #         return s/( (2*radius+1) * (radius+1) -1 )
 
 
-        def partial_edge_stability(edge, no_move=False):
-                if is_filled(edge):
-                    return sum( val for val in _static_edge_values)
-                max_stability = -inf
-                l=b.legal_moves()
-                if l[0][1:] == [-1,-1]:
-                    b.push(l[0])
-                    curr_stability = partial_edge_stability(edge, no_move=True)
-                moves = [move for move in  if move in edge[0]
-                curr_stability = probability(edge, pos)
-        def get_edges():
-            print("################# GET EDGES##################")
-            edges=[]
-            #the northen edge
-            edges.append([ [[0,i] for i in range(b._boardsize)], b._board[0]])
-            #print("#")
-            #print(b._board)
-            #print(edges[0])
-            #print("#")
-            #the eastern edge
-            l=[]
-            for i in range(b._boardsize):
-                l.append(b._board[i][-1])
-            edges.append( [[[i, b._boardsize] for i in range(b._boardsize)], l])
-            #print(edges[1])
-            #the southern edge
-            edges.append([ [[b._boardsize,i] for i in range(b._boardsize)], b._board[-1]])
-            #print(edges[2])
-            #the western edge
-            l=[]
-            for i in range(b._boardsize):
-                l.append(b._board[i][0])
-            edges.append( [[[i, 0] for i in range(b._boardsize)], l])
-            #print(edges)
-            print("################# GET EDGES##################")
+    #     def partial_edge_stability(edge, no_move=False):
+    #             if is_filled(edge):
+    #                 return sum( val for val in _static_edge_values)
+    #             max_stability = -inf
+    #             l=b.legal_moves()
+    #             if l[0][1:] == [-1,-1]:
+    #                 b.push(l[0])
+    #                 curr_stability = partial_edge_stability(edge, no_move=True)
+    #             moves = [move for move in  if move in edge[0]
+    #             curr_stability = probability(edge, pos)
+    #     def get_edges():
+    #         print("################# GET EDGES##################")
+    #         edges=[]
+    #         #the northen edge
+    #         edges.append([ [[0,i] for i in range(b._boardsize)], b._board[0]])
+    #         #print("#")
+    #         #print(b._board)
+    #         #print(edges[0])
+    #         #print("#")
+    #         #the eastern edge
+    #         l=[]
+    #         for i in range(b._boardsize):
+    #             l.append(b._board[i][-1])
+    #         edges.append( [[[i, b._boardsize] for i in range(b._boardsize)], l])
+    #         #print(edges[1])
+    #         #the southern edge
+    #         edges.append([ [[b._boardsize,i] for i in range(b._boardsize)], b._board[-1]])
+    #         #print(edges[2])
+    #         #the western edge
+    #         l=[]
+    #         for i in range(b._boardsize):
+    #             l.append(b._board[i][0])
+    #         edges.append( [[[i, 0] for i in range(b._boardsize)], l])
+    #         #print(edges)
+    #         print("################# GET EDGES##################")
 
-        def is_filled(edge):
-            for c in get_edge(edge):
-                if c == b.board._EMPTY:
-                    return False
-            return True
+    #     def is_filled(edge):
+    #         for c in get_edge(edge):
+    #             if c == b.board._EMPTY:
+    #                 return False
+    #         return True
 
-        get_edges()
-        return 1
+    #     get_edges()
+    #     return 1
 
 
     def disks(self, b):
@@ -298,5 +311,5 @@ class Stockfish(PlayerInterface):
         return b._nbBLACK - b._nbWHITE
 
     def heuristics(self, b):
-        value = self.mobility(b)+self.corners(b)+self.disks(b)+self.edge_stability(b)
+        value = self.mobility(b)+self.corners(b)+5*self.disks(b)#+self.edge_stability(b)
         return value
